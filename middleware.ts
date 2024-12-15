13e63dbee73ac73ed7d1134e8033e38971d091db
theeliteclub2k <vanjregautham@gmail.com>
2024-12-15 15:09:57 +0530

    fix: build error

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
-export default function middleware(req) {
+export default function middleware(req: Request) {
   return withAuth(req);
 }
 export const config = {
